b760bb655a30ef5908eb1337954d258fd0606674
brett-baumert <brett-baumert@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-03-28 20:40:41 +0000

    start of MutationTool.java
    git-svn-id: https://cjfileserver/svn/teh_code@5 93af362e-f9d5-9440-b008-9d94dd279ff6

---
+++ trunk/mutation_tool/src/MutationTool.java
@@ -0,0 +1,36 @@
+/**
+ * MutationTool is the main class and GUI for the mutation tool
+ * 	designed for csce 361
+ * 
+ * CSCE 361
+ * 
+ * @author teh_code
+ * @version 1.0
+ */
+
+//------------------------------------------
+// Import Statements
+//------------------------------------------
+
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.lang.*;
+
+public class MutationTool extends JFrame implements ActionListener
+{
+	//------------------------------------------
+	// Variables
+	//------------------------------------------
+	
+	
+	
+	//------------------------------------------
+	// GUI declarations
+	//------------------------------------------
+	
+	//
+	
+}
