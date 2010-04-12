5b60982ab2ced7105b5005f74f753cd89a720671
curtis-johnson <curtis-johnson@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-12 05:46:55 +0000

    Event Firing when a new node is selected.
    git-svn-id: https://cjfileserver/svn/teh_code@14 93af362e-f9d5-9440-b008-9d94dd279ff6

--- trunk/mutation_tool/src/controls/MutableTree.java
+++ trunk/mutation_tool/src/controls/MutableTree.java
@@ -1,20 +1,29 @@
 package controls;
 
+import java.io.File;
 import java.util.ArrayList;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeSelectionModel;
 
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.Method;
 
-public class MutableTree extends JTree {
+public class MutableTree extends JTree implements TreeSelectionListener {
 	
 	private ArrayList<JavaClass> alMutableClasses;
 	private DefaultTreeModel oMutableModel;
+	private DefaultTreeCellRenderer oMutableRenderer;
 	
 	public MutableTree() {
 		oMutableModel = new DefaultTreeModel(null);
+		oMutableRenderer = new DefaultTreeCellRenderer();
 	}
 	
 	public void setMutableObjects(ArrayList<JavaClass> alObjects) {
@@ -26,6 +35,7 @@ public class MutableTree extends JTree {
 		MutableNode mutableClassNode;
 		MutableNode mutableMethodNode;
 		Method[] arMethods;
+		
 		for (int i=0; i<alMutableClasses.size(); i++) {
 			mutableClassNode = new MutableNode(alMutableClasses.get(i));
 			arMethods = mutableClassNode.getMutableClass().getMethods();
@@ -36,6 +46,29 @@ public class MutableTree extends JTree {
 			//	exist only when a package is opened?
 			oMutableModel = new DefaultTreeModel(mutableClassNode);
 		}
+
+//		this.oMutableRenderer.setOpenIcon(imgMethod);
+//		this.oMutableRenderer.setClosedIcon(imgMethod);
+//		this.oMutableRenderer.setLeafIcon(imgMethod);
+		this.setCellRenderer(oMutableRenderer);
 		this.setModel(oMutableModel);
-	}	
+		this.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+		
+		this.addTreeSelectionListener(this);
+		
+	}
+
+	@Override
+	/**
+	 * This method is called when a new tree node is selected.
+	 */
+	public void valueChanged(TreeSelectionEvent e) {
+		MutableNode oSelectedNode = (MutableNode)this.getLastSelectedPathComponent();		
+		if (oSelectedNode.getMutableMethod() == null) {
+			System.out.println("Selected: " + oSelectedNode.getMutableClass().getClassName());
+		} else {
+			System.out.println("Selected: " + oSelectedNode.getMutableClass().getClassName() + "." + oSelectedNode.getMutableMethod().getName());
+		}		
+	}
+
 }
