3fe98d02b6421b9a9a84bf7f7ded4f3b7d803e39
curtis-johnson <curtis-johnson@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-15 05:14:34 +0000

    git-svn-id: https://cjfileserver/svn/teh_code@18 93af362e-f9d5-9440-b008-9d94dd279ff6

--- trunk/mutation_tool/src/controls/MutableTree.java
+++ trunk/mutation_tool/src/controls/MutableTree.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import javax.swing.JTree;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeSelectionModel;
@@ -11,32 +12,40 @@ import javax.swing.tree.TreeSelectionModel;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.Method;
 
+/**
+ * MutableTree is used in the Graphical User Interface portion of the mutation tool.
+ * This tree will display classes in a tree format, as well as their individual methods.
+ * 
+ * @author teh_code
+ *
+ */
+// TODO Edit this class, so when adding a new class and such, things are displayed in order in the tree.
 public class MutableTree extends JTree implements TreeSelectionListener {
 	
 	private ArrayList<JavaClass> alMutableClasses;
 	private DefaultTreeModel oMutableModel;
 	private DefaultTreeCellRenderer oMutableRenderer;
+	private DefaultMutableTreeNode oTreeRootNode;
 	
 	public MutableTree() {
 		oMutableModel = new DefaultTreeModel(null);
 		oMutableRenderer = new DefaultTreeCellRenderer();
 		this.alMutableClasses = new ArrayList<JavaClass>();
+		this.oTreeRootNode = new DefaultMutableTreeNode("No classes loaded.");
+		this.addTreeSelectionListener(this);
 	}
 	
-	public void addMutableClass(JavaClass oJavaClass) {
+	public void addMutableClassToTree(JavaClass oJavaClass) {
 		JavaClass oClassToAdd = oJavaClass.copy();
 		this.alMutableClasses.add(oClassToAdd);
 		drawMutableObjects();
 	}
-	/*
-	public void setMutableObjects(ArrayList<JavaClass> alObjects) {
-		this.alMutableClasses = alObjects;
-		drawMutableObjects();
-	}*/
 	
 	private void clearTree() {
-		oMutableModel = new DefaultTreeModel(null);
+		//oMutableModel = new DefaultTreeModel(null);
+		oMutableModel = new DefaultTreeModel(oTreeRootNode);
 		oMutableRenderer = new DefaultTreeCellRenderer();
+		oTreeRootNode.removeAllChildren();
 	}
 	
 	private void drawMutableObjects() {
@@ -44,15 +53,18 @@ public class MutableTree extends JTree implements TreeSelectionListener {
 		Method[] arMethods;
 		clearTree();
 		
+		if (alMutableClasses.size() == 0)
+			this.setRootVisible(true);
+		else
+			this.setRootVisible(false);		
+		
 		for (int i=0; i<alMutableClasses.size(); i++) {
 			mutableClassNode = new MutableNode(alMutableClasses.get(i));
 			arMethods = mutableClassNode.getMutableClass().getMethods();
 			for (int j=0; j < arMethods.length; j++) {
 				new MutableNode(arMethods[j], mutableClassNode);
 			}
-			//TODO Remove this, and figure out a way to add multiple classes to a model...or will multiple classes
-			//	exist only when a package is opened?
-			oMutableModel = new DefaultTreeModel(mutableClassNode);
+			oTreeRootNode.add(mutableClassNode);
 		}
 
 //		this.oMutableRenderer.setOpenIcon(imgMethod);
@@ -60,10 +72,7 @@ public class MutableTree extends JTree implements TreeSelectionListener {
 //		this.oMutableRenderer.setLeafIcon(imgMethod);
 		this.setCellRenderer(oMutableRenderer);
 		this.setModel(oMutableModel);
-		this.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-		
-		this.addTreeSelectionListener(this);
-		
+		this.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);		
 	}
 
 	@Override
