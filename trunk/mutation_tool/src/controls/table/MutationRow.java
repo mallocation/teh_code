979dccf7b6bae0bd823ceb95abaae236779afbda
curtis-johnson <curtis-johnson@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-17 15:04:52 +0000

    git-svn-id: https://cjfileserver/svn/teh_code@29 93af362e-f9d5-9440-b008-9d94dd279ff6

---
+++ trunk/mutation_tool/src/controls/table/MutationRow.java
@@ -0,0 +1,138 @@
+package controls.table;
+
+import interfaces.IMutableObject;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+
+public class MutationRow extends JPanel implements ActionListener {
+	
+	JLabel lblMutationName;
+	JCheckBox chkCreateMutation;	
+	JLabel lblMutationType;
+	JLabel lblMutationOps;
+	JLabel lblMutationLevel;
+	
+	Font oMutationNameFont;
+	Font oPropertiesFont;
+	
+	Dimension oMutationNameDim, oPropertiesDim, oRowDimension;
+	
+	Color rowBgColor;
+	
+	
+
+	public MutationRow(String mutationName, boolean bCreateMutation, String mutationType, String oldOperator,
+					   	String newOperator, String mutationLevel, boolean bAltRow, ActionListener oTableListener) {
+		lblMutationName = new JLabel(mutationName);
+		chkCreateMutation = new JCheckBox();
+		chkCreateMutation.setSelected(bCreateMutation);
+		lblMutationType = new JLabel("Type: " + mutationType);
+		lblMutationOps = new JLabel("Mutates: " + oldOperator + " to " + newOperator);
+		lblMutationLevel = new JLabel("Level: " + mutationLevel);
+		
+		oMutationNameFont = new Font(Font.SANS_SERIF, Font.PLAIN, 14);
+		oPropertiesFont = new Font(Font.SANS_SERIF, Font.ITALIC, 10);
+		
+		oMutationNameDim = new Dimension(360, 20);
+		oPropertiesDim = new Dimension(150,20);
+		oRowDimension = new Dimension(400, 50);
+		
+		rowBgColor = bAltRow ? new Color(238, 238, 238) : new Color(255, 255, 255);
+		
+		chkCreateMutation.addActionListener(oTableListener);
+		
+		drawMutantRow();
+	}
+	
+	public void drawMutantRow() {
+		lblMutationName.setFont(oMutationNameFont);
+		lblMutationName.setPreferredSize(oMutationNameDim);
+		
+		lblMutationType.setFont(oPropertiesFont);
+		lblMutationType.setPreferredSize(oPropertiesDim);
+		
+		lblMutationOps.setFont(oPropertiesFont);
+		lblMutationOps.setPreferredSize(oPropertiesDim);
+		
+		lblMutationLevel.setFont(oPropertiesFont);
+		
+		this.setLayout(new FlowLayout(FlowLayout.LEFT));
+		this.setBackground(rowBgColor);
+		
+		this.add(lblMutationName);
+		this.add(chkCreateMutation);
+		this.add(lblMutationType);
+		this.add(lblMutationOps);
+		this.add(lblMutationLevel);
+		
+		this.setPreferredSize(oRowDimension);
+	}
+	
+	public boolean isSelected() {
+		return this.chkCreateMutation.isSelected();
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+
+
+
+
+//public class MutationRenderer extends JPanel implements ListCellRenderer {
+//	
+//	private JLabel lblMutationLevel;
+//	private JLabel lblMutationName;
+//	private JLabel lblMutationType;
+//	private JCheckBox chkSelectMutant;
+//	private IMutableObject oMutant;
+//	
+//	public MutationRenderer () {
+//		this.lblMutationLevel = new JLabel();
+//		this.lblMutationName = new JLabel();
+//		this.lblMutationType = new JLabel();
+//		this.chkSelectMutant = new JCheckBox();
+//	
+//		lblMutationName.setSize(new Dimension(600, 50));
+//		this.setLayout(new FlowLayout(FlowLayout.LEFT));
+//		this.add(lblMutationName);
+//		this.add(lblMutationType);
+//		this.add(lblMutationLevel);
+//		this.add(chkSelectMutant);
+//		
+//		
+//		this.setSize(600, 100);
+//		this.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+//	}
+//	
+//	@Override
+//	public Component getListCellRendererComponent(JList list, Object value,int index, boolean isSelected, boolean cellHasFocus) {
+//		this.oMutant = (IMutableObject)value;
+//		
+//		this.lblMutationLevel.setText("Level : " + oMutant.getMutantLevelAsString());
+//		this.lblMutationName.setText("Mutation!!!");
+//		this.lblMutationType.setText("Type : " + oMutant.getMutantTypeAsString());
+//		this.chkSelectMutant.setSelected(isSelected);
+//
+//		return this;		
+//	}
+//
+//}
--- trunk/mutation_tool/src/controls/table/MutationTable.java
+++ trunk/mutation_tool/src/controls/table/MutationTable.java
@@ -1,11 +1,61 @@
 package controls.table;
 
-import javax.swing.JComponent;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MutationTable extends JComponent {
-	
+import javax.swing.BoxLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+
+public class MutationTable extends JPanel implements ActionListener {
+
 	public MutationTable() {
+		this.setSize(400, 100);
 		
+		//panel2.add(chk2);
+		//panel2.setMinimumSize(new Dimension(400, 50));
+		//panel2.setPreferredSize(new Dimension(400, 50));
+		//panel2.setMaximumSize(new Dimension(400, 50));
+		for (int i=0; i<50000; i++) {
+			if (i%2 == 0) {
+				MutationRow oRow = new MutationRow("Name", false, "ARITHMETIC", "*", "/", "CLASS", false, this);
+				this.add(oRow);
+			} else {
+				MutationRow oRow = new MutationRow("Name", false, "Logical", "AND", "OR", "CLASS", true, this);
+				this.add(oRow);
+			}
+		}
+		
+		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		
+		
+	}
+	
+	public void getSelectedMutationCount() {
+		Component[] arComponents = this.getComponents();
+		int nSelected = 0;
+		for (int i=0; i<arComponents.length; i++) {
+			MutationRow oRow = (MutationRow)arComponents[i];
+			if (oRow.isSelected()) {
+				nSelected++;
+			}			
+		}
+		System.out.println(nSelected + " selected mutations.");
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		getSelectedMutationCount();		
 	}
 
 }
