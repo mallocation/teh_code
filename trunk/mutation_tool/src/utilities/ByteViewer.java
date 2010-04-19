d4a6e34f0346924e8b6b36f9f0674f11bbdff08e
brett-baumert <brett-baumert@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-19 21:48:22 +0000

    The ByteViewer class is now functional.  I fixed the problem with loading a class file of type JavaClass.
    git-svn-id: https://cjfileserver/svn/teh_code@40 93af362e-f9d5-9440-b008-9d94dd279ff6

--- trunk/mutation_tool/src/utilities/ByteViewer.java
+++ trunk/mutation_tool/src/utilities/ByteViewer.java
@@ -1,40 +1,24 @@
 package utilities;
 
-import java.io.*;
-
 import org.apache.bcel.classfile.*;  // The static component of the BCEL API 
-import org.apache.bcel.Repository;
+import utilities.ClassLoader;
 
-/* The ByteViewer is used to display a human-readable listing of the
-   compiled Java bytecode for a class.
+/* This class is used to get set create on object of a class to store
+ * bytecode of its class and methods.
 */ 
 
 public class ByteViewer {
 	
-	public JavaClass classClass;
-	public Method[] classMethods;
-	public Code classCode;
+	public JavaClass oClass;
+	public Method[] oClassMethods;
 
-  /* Throws IOException If there is an error reading from the class file.
-  */
-  public void generateByteCode(String className) {
-
-    try {
-      /* Load the class from CLASSPATH. 
-      */
-      JavaClass       clazz   = Repository.lookupClass(className);
-
-      /* Get data in a method. 
-      */ 
-      Method[]        methods = clazz.getMethods();
-
-      System.out.println(clazz);
-      for(int i=0; i < methods.length; i++) {
-	  System.out.println(methods[i]);
-	  Code code = methods[i].getCode();
-	  if(code != null)
-	      System.out.println(code);
-      }
+	// Throws IOException If there is an error reading from the class file.
+	public void generateByteCode(String className) {
+		try {
+			//set object class and methods
+			oClass = ClassLoader.loadClassFromPath(className);
+			oClassMethods = oClass.getMethods();
+			
     } catch (Exception e) { e.printStackTrace(); } 
   }
 }
