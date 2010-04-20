8b092c70e5d4fad3598410cb14dffcef2f3b6ccb
pavel-patino <pavel-patino@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-20 08:54:49 +0000

    git-svn-id: https://cjfileserver/svn/teh_code@41 93af362e-f9d5-9440-b008-9d94dd279ff6

---
+++ trunk/mutation_tool/src/mutations/ChangeOp.java
@@ -0,0 +1,304 @@
+package mutations;
+
+import java.util.*;
+import org.apache.bcel.Repository;
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.generic.*;
+import java.io.*;
+/**
+ * ChangeOp is a program that will perform dynamic operator mutations on a specific
+ * class file.  All instances of an arithmetic operator will be changed to the new operator
+ * specified at runtime.
+ *
+ * @author teh_code
+ * @version 1.0
+ *
+ */
+public class ChangeOp {
+	
+	/**
+	 * List of usable operators within the program.
+	 */
+	private static ArrayList<String> _alValidOperators;
+	
+	/**
+	 * List of arithmetic operators.
+	 */
+	private ArrayList<String> _alArithmeticOperators;
+	
+	/**
+	 * List of relational operators
+	 */
+	private ArrayList<String> _alRelationalOperators;
+	
+	/**
+	 * List of boolean operators
+	 */
+	private ArrayList<String> _alBooleanOperators;
+	
+	/**
+	 * Class to be mutated.  Specified at runtime.
+	 */
+	private JavaClass oClass;
+	
+	/**
+	 * ClassGen object used to generate the mutated class.
+	 */
+	private ClassGen oClassGen;
+	
+	/**
+	 * ConstantPool object of the class to modify.
+	 *
+	private ConstantPool oConstantPool;*/
+	
+	/**
+	 * ConstantPoolGen used in generating the mutated class.
+	 */
+	private ConstantPoolGen oConstantPoolGen;
+	
+	/**
+	 * Array of methods within the mutated class.
+	 */
+	private Method[] arMethods;
+	
+	/**
+	 * The operator to be mutated.
+	 */
+	private String sOldOperator;
+	
+	/**
+	 * The new operator resulting from mutation.
+	 */
+	private String sNewOperator;
+	
+	/**
+	 * The instruction helper used for generating mutated instructions.
+	 */
+	private cInstructionHelper oInstHelper;
+	
+	/**
+	 * This will create an instance of the ChangeOp program.
+	 *
+	 * @param args command line arguments of main method containing class file and operators
+	 */
+	public ChangeOp(String args[]) {
+		//parse the arguments (class file, operators) and create an instruction helper
+		parseArguments(args);
+		oInstHelper = new cInstructionHelper();
+	}
+	
+	/**
+	 * This is a readonly property that contains a list of all valid operators that may be modified within a class.
+	 * This readonly property provides for an easy way to quickly add/remove operators for use.
+	 *
+	 * @return <code>ArrayList</code> containing all operators available for modification.
+	 */
+	private ArrayList<String> alValidOperators() {
+		//if the list is null, initialize it
+		if (_alValidOperators == null) {
+			_alValidOperators = new ArrayList<String>();
+			_alValidOperators.add("+");_alValidOperators.add("-");_alValidOperators.add("*");_alValidOperators.add("/");_alValidOperators.add("%");										//Arithmetic
+			_alValidOperators.add("==");_alValidOperators.add("!=");_alValidOperators.add("<");_alValidOperators.add(">");_alValidOperators.add("<=");_alValidOperators.add(">=");		//Relational Operators
+			_alValidOperators.add("&&");_alValidOperators.add("||");		//booleans
+			
+		}
+		return _alValidOperators;
+	}
+	
+	/**
+	 * This list contains all valid boolean operators that may be mutated in a class.
+	 * @return <code>ArrayList</code> containing all boolean operators available for mutations.
+	 */
+	private ArrayList<String> alBooleanOperators(){
+		if(_alBooleanOperators == null){
+			_alBooleanOperators.add("&&");_alBooleanOperators.add("||");
+		}
+		return _alBooleanOperators;
+	}
+	
+	/**
+	 * This list contains all valid relational operators that may be mutated in a class.
+	 * @return <code>ArrayList</code> containing all relational operators available for mutations.
+	 */
+	private ArrayList<String> alRelationalOperators(){
+		if(_alRelationalOperators == null){
+			_alRelationalOperators.add("==");_alRelationalOperators.add("!=");_alRelationalOperators.add("<=");
+			_alRelationalOperators.add(">=");_alRelationalOperators.add("<");_alRelationalOperators.add(">");
+		}
+		return _alRelationalOperators;
+	}
+	
+	/**
+	 * This list contains all valid arithmetic operators that may be mutated in a class.
+	 * @return <code>ArrayList</code> containing all arithmetic operators available for mutations.
+	 */
+	private ArrayList<String> alArithmeticOperators(){
+		if(_alArithmeticOperators == null){
+			_alArithmeticOperators.add("+");_alArithmeticOperators.add("-");_alArithmeticOperators.add("*");_alArithmeticOperators.add("/");_alArithmeticOperators.add("%");
+		}
+		return _alArithmeticOperators;
+	}
+	
+	/**
+	 * Determines if sOperator is an element in the list alOperatorsList. The list itself
+	 * will determine the type of operator you are checking sOperator to be.
+	 * @param <code>alOperatorsList</code> The list of valid operators to be checked.
+	 * @param <code>sOperator</code> The operator to be tested against the valid operators list.
+	 * @return <code> true </code> if the operator is in the valid operators list 'alOperatorsList'
+	 * 		   <code> false </code> otherwise
+	 */
+	private boolean isValidOperator(ArrayList<String> alOperatorsList, String sOperator){
+		if(alOperatorsList.indexOf(sOperator) != -1){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	
+	/**
+	 * Parses the arguments passed from the "main" method.
+	 * These arguments should contain the class name, and the operators.
+	 *
+	 * @param args program execution command line arguments
+	 */
+	private void parseArguments(String[] args) {
+		//Get the class file
+		try {
+			oClass = Repository.lookupClass(args[0]);
+			oClassGen = new ClassGen(oClass);
+		} catch (Exception e) {
+			showError("Please specify a valid class file.");
+		}
+		
+		//Get the operators
+		try {
+			//Store the operators, and trim any single quotes (needed to pass *, <, >, etc. as an argument)
+			sOldOperator = args[1].replace("'", "");
+			sNewOperator = args[2].replace("'", "");
+			boolean operatorTypesMismatch = true;								//Having sOldOperator equal '*' and sNewOperator equal '>' is an example of type mismatch.
+			/*
+			if (sOldOperator.equals(sNewOperator)) {
+				showError("Old and New operators cannot be the same operator.");
+			}
+			else if (!isValidOperator(alValidOperators(), sOldOperator) || !isValidOperator(alValidOperators(), sNewOperator)) {
+				showError("Invalid operators specified.");
+			}
+			else if ( isValidOperator(alArithmeticOperators(), sOldOperator) && isValidOperator(alArithmeticOperators(), sNewOperator)) {
+				operatorTypesMismatch = false;
+			}
+			else if ( isValidOperator(alBooleanOperators(), sOldOperator) && isValidOperator(alBooleanOperators(), sNewOperator)){
+				operatorTypesMismatch = false;
+			}
+			else if ( isValidOperator(alRelationalOperators(), sOldOperator) && isValidOperator(alRelationalOperators(), sNewOperator)){
+				operatorTypesMismatch = false;
+			}
+			else if (operatorTypesMismatch){
+				showError("Operators type mismatch. Cannot have different types of operators (e.g., + and ==).");
+			}*/
+			
+			if (!isValidOperator(alValidOperators(), sOldOperator) || !isValidOperator(alValidOperators(), sNewOperator)) {
+				showError("Invalid operators specified.");
+			}
+			if (sOldOperator.equals(sNewOperator)) {
+				showError("Old and New operators cannot be the same operator.");
+			}
+		} catch (Exception e) {
+			showError("Please specify an old operator and a new operator.");
+		}
+	}
+	
+	/**
+	 * Prints the usage specifications of the program.
+	 */
+	private void printUsage() {
+		System.out.println("<Usage> java ChangeOp <class-file> <oldOp> <newOp>");
+		System.out.println("<class-file>: without extension");
+		System.out.println("<oldOp>: See Manual for Details");
+		System.out.println("<newOp>: See Manual for Details");
+	}
+	
+	/**
+	 * Shows an error to the user, prints the program usage, and exits.
+	 * 
+	 * @param sError the error to display to the user
+	 */
+	private void showError(String sError) {
+		System.out.println(sError);
+		printUsage();
+		System.exit(1);
+	}
+	
+	/**
+	 * Changes all instances of the old operator to the new operator
+	 * within a specific class file.
+	 */
+	public void changeOperators() {
+		int nMutations = 0;
+		System.out.println("Changing all instances of \"" + sOldOperator + "\" to \"" + sNewOperator + "\" in " + oClass.getClassName());
+		
+		oConstantPoolGen = oClassGen.getConstantPool();
+		arMethods = oClassGen.getMethods();
+		
+		for (int i=0; i<arMethods.length; i++) {
+			// mutation process
+			// 1. Go through all instructions in a method
+			// 2. If an instruction is an arithmetic instruction, see if it matches the old operator
+			// 3. If it matches the old operator, change the instruction to match the new operator, and set the isntruction
+			// 4. otherwise, don't worry about it and move on!
+			
+			MethodGen oMethodGen = new MethodGen(arMethods[i], oClass.getClassName(), oConstantPoolGen);
+			InstructionList il = oMethodGen.getInstructionList();
+			InstructionHandle ih;
+			for (ih = il.getStart(); ih != null; ih = ih.getNext()) {
+				Instruction oInstruction = ih.getInstruction();
+				
+				if (oInstHelper.isArithmeticInstruction(oInstruction)) {
+					if (oInstHelper.InstructionMatchOperator(oInstruction, sOldOperator)) {
+						//convert to the new instruction
+						oInstruction = oInstHelper.ConvertArithmetic(oInstruction, sNewOperator);
+						ih.setInstruction(oInstruction);					
+						nMutations++;
+					}					
+				}
+				else if(oInstHelper.isBranchInstruction(oInstruction)){
+					BranchInstruction oBranchInstruction = (BranchInstruction) oInstruction;
+					if (oInstHelper.InstructionMatchOperator(oInstruction, sOldOperator)) {
+						oBranchInstruction = oInstHelper.ConvertBranch(oBranchInstruction, sNewOperator);
+						ih.setInstruction(oBranchInstruction);
+						nMutations++;
+					}
+				}
+			}
+			il.setPositions();
+			oMethodGen.setInstructionList(il);
+			oClassGen.setMethodAt(oMethodGen.getMethod(), i);
+			il.dispose();
+		}		
+		System.out.println("Mutation complete. " + nMutations + " mutation(s) were performed.");	
+	}	
+	
+	/**
+	 * Dumps the modified class to the program directory.
+	 */
+	public void dumpClass() {
+		try {
+			oClassGen.getJavaClass().dump(oClass.getClassName() + ".class");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	/**
+	 * Offers the main method of program execution.  This is where
+	 * a call to changeOperators and dumpClass should be made.
+	 *
+	 * @param args command line arguments
+	 */
+	public static void main(String args[]) {
+		ChangeOp oOperatorChanger = new ChangeOp(args);
+		oOperatorChanger.changeOperators();
+		oOperatorChanger.dumpClass();
+	}
+}
---
+++ trunk/mutation_tool/src/mutations/cInstructionHelper.java
@@ -0,0 +1,310 @@
+package mutations;
+
+import java.util.*;
+import org.apache.bcel.generic.Instruction;
+import org.apache.bcel.generic.ArithmeticInstruction;
+import org.apache.bcel.generic.InstructionFactory;
+import org.apache.bcel.generic.BranchInstruction;
+import org.apache.bcel.generic.InstructionHandle;
+import org.apache.bcel.Constants;
+
+/**
+ * cInstructionHelper is a helper class for the ChangeOp program.
+ * This class handles all operator/instruction verification.
+ *
+ * @author teh_code
+ * @version 1.0
+ */
+public class cInstructionHelper {
+	
+	/**
+	 * Valid arithmetic instructions of type DOUBLE.
+	 */
+	private ArithmeticInstruction[] arDoubleInstructions = {InstructionFactory.DADD, InstructionFactory.DSUB, InstructionFactory.DMUL, InstructionFactory.DDIV, InstructionFactory.DREM};
+	
+	/**
+	 * Valid arithmetic instructions of type FLOAT.
+	 */
+	private ArithmeticInstruction[] arFloatInstructions = {InstructionFactory.FADD, InstructionFactory.FSUB, InstructionFactory.FMUL, InstructionFactory.FDIV, InstructionFactory.FREM};
+	
+	/**
+	 * Valid arithmetic instructions of type INT.
+	 */
+	private ArithmeticInstruction[] arIntInstructions = {InstructionFactory.IADD, InstructionFactory.ISUB, InstructionFactory.IMUL, InstructionFactory.IDIV, InstructionFactory.IREM};
+	
+	/**
+	 * Valid arithmetic instructions of type LONG.
+	 */
+ 	private ArithmeticInstruction[] arLongInstructions = {InstructionFactory.LADD, InstructionFactory.LSUB, InstructionFactory.LMUL, InstructionFactory.LDIV, InstructionFactory.LREM};
+	
+	/**
+	 * Valid branch instructions of type INT
+	 */
+	private ArrayList<String> al_IntBranchInstructions;
+	
+	/**
+	 * 'A' type branch instructions compare object references.
+	 */
+	private ArrayList<String> al_ABranchInstructions;
+	
+	/**
+	 * BranchInstructions instructions that compare to zero/null and branch accordingly.
+	 */
+	private ArrayList<String> al_ZeroNullBranchInstructions;
+	
+	/**
+	 * The constructor initializes all of the ArrayLists
+	 */
+	public cInstructionHelper(){
+		al_IntBranchInstructions = new ArrayList<String>();
+		al_ABranchInstructions = new ArrayList<String>();
+		al_ZeroNullBranchInstructions = new ArrayList<String>();
+		al_IntBranchInstructions.add("if_icmpeq");al_IntBranchInstructions.add("if_icmpne");al_IntBranchInstructions.add("if_icmplt");al_IntBranchInstructions.add("if_icmple");
+		al_IntBranchInstructions.add("if_icmpgt");al_IntBranchInstructions.add("if_icmpge");
+		
+		al_ABranchInstructions.add("if_acmpeq");al_ABranchInstructions.add("if_acmpne");
+		
+		al_ZeroNullBranchInstructions.add("ifeq");al_ZeroNullBranchInstructions.add("ifne");al_ZeroNullBranchInstructions.add("iflt");al_ZeroNullBranchInstructions.add("ifle");
+		al_ZeroNullBranchInstructions.add("ifgt");al_ZeroNullBranchInstructions.add("ifge");al_ZeroNullBranchInstructions.add("ifnull");al_ZeroNullBranchInstructions.add("ifnonull");
+	}
+	
+	
+	/**
+	 * Checks if a specific Instruction in the Java virtual machine is an Arithmetic Instruction.
+	 *
+	 * @param oInstruction the instruction to check
+	 * 
+	 * @return <code>true</code> if the instruction is an arithmetic instruction,
+	 *         <code>false</code> otherwise
+	 */
+	public boolean isArithmeticInstruction(Instruction oInstruction) {
+		if (oInstruction instanceof ArithmeticInstruction) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * Checks if a specific instruction is of type Branch.
+	 * @param oInstruction
+	 * @return
+	 */
+	public boolean isBranchInstruction(Instruction oInstruction){
+		String instructionName = oInstruction.getName();
+		if(al_IntBranchInstructions.indexOf(instructionName) != -1){
+			return true;
+		}
+		else if(al_ABranchInstructions.indexOf(instructionName) != -1){
+			return true;
+		}
+		else if(al_ZeroNullBranchInstructions.indexOf(instructionName) != -1){
+			return true;
+		}else{
+			return false;
+		}
+	}
+			
+	
+	/**
+	 * Checks if a specific instruction is that of the operator type.
+	 * i.e. IADD corresponds to the "+" operator, and would therefore return true
+	 *
+	 * @param oInstruction the instruction to examine
+	 * @param sOperator the desired operator to match
+	 *
+	 * @return <code>true</code> if the instructions is representative of the given operator,
+	 *         <code>false</code> otherwise
+	 */
+	public boolean InstructionMatchOperator(Instruction oInstruction, String sOperator) {
+	//	System.out.println("" + oInstruction.getName() );
+		return oInstruction.getName().contains(OperatorToInstName(sOperator));
+	}
+	
+	/**
+	 * Converts an Arithmetic Instruction to that of the new operator.
+	 *
+	 * @param oInstruction the instruction to convert
+	 * @param sNewOperator operator representative of the new operation
+	 *
+	 * @return the new arithmetic instruction corresponding to the new operation type
+	 */
+	public Instruction ConvertArithmetic(Instruction oInstruction, String sNewOperator) {
+		//First get the arithmetic type to know what instruction type to convert to
+		eArithmeticType oArithType = InstructionToArithmeticType(oInstruction);
+		Instruction oNewInstruction = null; //The converted instruction
+		
+		//Now, we have the arithmetic type, get the corresponding opcode.
+		switch (oArithType) {
+			case DOUBLE:	oNewInstruction = findArithmeticInstructionByName(arDoubleInstructions, OperatorToInstName(sNewOperator));break;
+			case FLOAT:		oNewInstruction = findArithmeticInstructionByName(arFloatInstructions, OperatorToInstName(sNewOperator));break;
+			case INT:		oNewInstruction = findArithmeticInstructionByName(arIntInstructions, OperatorToInstName(sNewOperator));break;
+			case LONG:		oNewInstruction = findArithmeticInstructionByName(arLongInstructions, OperatorToInstName(sNewOperator));break;
+		}		
+		return oNewInstruction;
+	}
+	
+	/**
+	 * Converts a branch instruction to that of the new operator.
+	 * @param oInstruction
+	 * @param sNewOperator
+	 * @return
+	 */
+	public BranchInstruction ConvertBranch(BranchInstruction oInstruction, String sNewOperator){
+		eBranchType oBranchType = InstructionToBranchType(oInstruction);
+		BranchInstruction oNewInstruction = null;
+		
+		switch(oBranchType) {
+			case INT: oNewInstruction = findBranchInstructionByName(al_IntBranchInstructions, OperatorToInstName(sNewOperator), oInstruction);break;
+			case A: oNewInstruction = findBranchInstructionByName(al_ABranchInstructions, OperatorToInstName(sNewOperator), oInstruction);break;
+			case ZERO: oNewInstruction = findBranchInstructionByName(al_ZeroNullBranchInstructions, OperatorToInstName(sNewOperator), oInstruction);break;
+		}
+		return oNewInstruction;
+	}
+	
+	/**
+	 * Determines an operator's corresponding instruction name. The input should be parsed
+	 * so that this function will always return one of: add, sub, ... , ge, gt.  
+	 *
+	 * @param sOperator the operator passed
+	 *
+	 * @return the name of the operator character
+	 */
+	private String OperatorToInstName(String sOperator) {
+		if (sOperator.equals("+")) { return "add"; }
+		else if (sOperator.equals("-")) { return "sub"; }
+		else if (sOperator.equals("*")) { return "mul"; }
+		else if (sOperator.equals("/")) { return "div"; }
+		else if (sOperator.equals("%")) { return "rem"; }
+		else if (sOperator.equals("==")) { return "ne"; }
+		else if (sOperator.equals("!=")) { return "eq"; }
+		else if (sOperator.equals(">") ) { return "le"; }
+		else if (sOperator.equals(">=")) { return "lt"; }
+		else if (sOperator.equals("<") ) { return "ge"; }
+		else if (sOperator.equals("<=")) { return "gt"; }
+		else { return ""; }
+	}
+	
+	/**
+	 * Searches for an instruction in the specified array by a subset of the
+	 * instruction's name.
+	 *
+	 * @param arInstructions array of instructions
+	 * @param sSubName subset of the instruction name
+	 *
+	 * @return <code>Instruction</code> if an instruction containing sSubName in the name is found,
+	 *         <code>null</code> otherwise
+	 */
+	private Instruction findArithmeticInstructionByName(Instruction arInstructions[], String sSubName) {
+		for (int i=0; i<arInstructions.length; i++) {
+			if (arInstructions[i].getName().toLowerCase().contains(sSubName.toLowerCase())) {
+				//If the instruction name contains sSubName, return it
+				return arInstructions[i];
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * This function does the same the same thing as findArithmeticInstructionByname() except for branch instructions
+	 * @param al_Instructions  
+	 * @param sSubName
+	 * @param oBranchInstruction
+	 * @return oNewBranchInstruction 
+	 */
+	private BranchInstruction findBranchInstructionByName(ArrayList<String> al_Instructions, String sSubName, BranchInstruction oBranchInstruction){
+		String opCode = "";
+		BranchInstruction oNewBranchInstruction = null;
+		
+		for(int i = 0; i < al_Instructions.size(); i++){
+			if(al_Instructions.get(i).toLowerCase().contains(sSubName.toLowerCase())){
+				opCode = al_Instructions.get(i);
+				oNewBranchInstruction = InstructionFactory.createBranchInstruction(getOpcodeNumber(opCode), oBranchInstruction.getTarget());
+				return oNewBranchInstruction;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * This function retrieves the op code number from the Constants class in the bcel library.
+	 * 
+	 * @param sOpcode The name of the opcode as a string
+	 * @return opCode The numerical value of the opcode
+	 */
+	private short getOpcodeNumber(String sOpcode){
+		short opCode = 0;
+		for(int i = 0; i < Constants.OPCODE_NAMES.length; i++){
+			if(Constants.OPCODE_NAMES[i].toLowerCase().equals(sOpcode.toLowerCase()) ){
+				opCode = (short)i;
+			}
+		}
+		return opCode;
+	}
+	
+
+	/**
+	 * Determines the arithmetic type of a specific instruction. (Double, Int, etc.)
+	 * isArithmeticInstruction should be called prior to this
+	 *
+	 * @param oInstruction the instruction to check
+	 * @return <code>eArithmeticType</code> if a valid arithmetic type is found,
+	 *         <code>null</code> otherwise
+	 */
+	private eArithmeticType InstructionToArithmeticType(Instruction oInstruction) {
+		if (Arrays.asList(arDoubleInstructions).indexOf(oInstruction) != -1) {
+			return eArithmeticType.DOUBLE;
+		} else if (Arrays.asList(arFloatInstructions).indexOf(oInstruction) != -1) {
+			return eArithmeticType.FLOAT;
+		} else if (Arrays.asList(arIntInstructions).indexOf(oInstruction) != -1) {
+			return eArithmeticType.INT;
+		} else if (Arrays.asList(arLongInstructions).indexOf(oInstruction) != -1) {
+			return eArithmeticType.LONG;
+		} else {
+			return null; //should never get here
+		}
+	}		
+	
+	/**
+	 * Determines the branch type of an instruction passed to the function. 
+	 * The types can be INT - integer comparisons, A - object comparisons
+	 * or Zero - Comparing to zero or to null (NOTE: MAY NEED TO EDIT THIS TO HAVE DIFFERENT TYPES FOR ZERO AND NULL)
+	 * 
+	 * @param oInstruction the instruction from which the branch type is derived
+	 * @return eBranchType
+	 */
+	private eBranchType InstructionToBranchType(Instruction oInstruction){
+		String sInstructionName = oInstruction.getName();
+		if(al_IntBranchInstructions.indexOf(sInstructionName) != -1){
+			return eBranchType.INT;
+		}
+		else if (al_ABranchInstructions.indexOf(sInstructionName) != -1){
+			return eBranchType.A;
+		}
+		else if (al_ZeroNullBranchInstructions.indexOf(sInstructionName) != -1){
+			return eBranchType.ZERO;
+		}
+		else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Enumeration for valid arithmetic operation types.
+	 */
+	private enum eArithmeticType {
+		DOUBLE,
+		FLOAT,
+		INT,
+		LONG
+	}
+	
+	/**
+	 * Enumeration for valid branch operation types.
+	 */
+	private enum eBranchType {
+		INT,
+		A,
+		ZERO
+	}
+}
