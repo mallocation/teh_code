c907b5874a09436645b2efe4c69d048d77c5630d
pavel-patino <pavel-patino@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-23 07:57:50 +0000

    git-svn-id: https://cjfileserver/svn/teh_code@59 93af362e-f9d5-9440-b008-9d94dd279ff6

---
+++ trunk/mutation_tool/src/mutations/Mutator.java
@@ -0,0 +1,300 @@
+package mutations;
+
+import java.util.*;
+import org.apache.bcel.Repository;
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.generic.*;
+/**
+ * Mutator performs dynamic operator mutations on a specific
+ * class package, class file, or method in a class.  Its purpose is to change
+ * all instances of some old relational, boolean, or arithmetic operator to a new
+ * operator of the proper corresponding kind (i.e., arithmetic-arithmetic). It is used
+ * in conjunction with the cInstructionHelper class.
+ * 
+ * specified at runtime (for now -> Needs to be passed an object to work with mutation_tool
+ *
+ * @author teh_code
+ * @version 1.0
+ *
+ */
+public class Mutator {
+	
+	/**
+	 * List of usable operators within the program.
+	 */
+	private static ArrayList<String> _alValidOperators;
+	
+	/**
+	 * List of arithmetic operators.
+	 */
+	private ArrayList<String> _alArithmeticOperators;
+	
+	/**
+	 * List of relational operators
+	 */
+	private ArrayList<String> _alRelationalOperators;
+	
+	/**
+	 * List of boolean operators
+	 */
+	private ArrayList<String> _alBooleanOperators;
+	
+	/**
+	 * Class to be mutated.  Specified at runtime.
+	 */
+	private JavaClass oClass;
+	
+	/**
+	 * ClassGen object used to generate the mutated class.
+	 */
+	private ClassGen oClassGen;
+	
+	/**
+	 * ConstantPoolGen used in generating the mutated class.
+	 */
+	private ConstantPoolGen oConstantPoolGen;
+	
+	/**
+	 * Array of methods within the mutated class.
+	 */
+	private Method[] arMethods;
+	
+	/**
+	 * The operator to be mutated.
+	 */
+	private String sOldOperator;
+	
+	/**
+	 * The new operator resulting from mutation.
+	 */
+	private String sNewOperator;
+	
+	/**
+	 * The instruction helper used for generating mutated instructions.
+	 */
+	private cInstructionHelper oInstHelper;
+	
+	/**
+	 * This will create an instance of the Mutator program.
+	 *
+	 * @param args command line arguments of main method containing class file and operators
+	 */
+	public Mutator(String args[]) {
+		//parse the arguments (class file, operators) and create an instruction helper
+		parseArguments(args);
+		oInstHelper = new cInstructionHelper();
+	}
+	
+	/**
+	 * This is a readonly property that contains a list of all valid operators that may be modified within a class.
+	 * This readonly property provides for an easy way to quickly add/remove operators for use.
+	 *
+	 * @return <code>ArrayList</code> containing all operators available for modification.
+	 */
+	private ArrayList<String> alValidOperators() {
+		//if the list is null, initialize it
+		if (_alValidOperators == null) {
+			_alValidOperators = new ArrayList<String>();
+			_alValidOperators.add("+");_alValidOperators.add("-");_alValidOperators.add("*");_alValidOperators.add("/");_alValidOperators.add("%");										//Arithmetic
+			_alValidOperators.add("==");_alValidOperators.add("!=");_alValidOperators.add("<");_alValidOperators.add(">");_alValidOperators.add("<=");_alValidOperators.add(">=");		//Relational Operators
+			_alValidOperators.add("&&");_alValidOperators.add("||");		//booleans
+			
+		}
+		return _alValidOperators;
+	}
+	
+	/**
+	 * This list contains all valid boolean operators that may be mutated in a class.
+	 * @return <code>ArrayList</code> containing all boolean operators available for mutations.
+	 */
+	private ArrayList<String> alBooleanOperators(){
+		if(_alBooleanOperators == null){
+			_alBooleanOperators = new ArrayList<String>();
+			_alBooleanOperators.add("&&");_alBooleanOperators.add("||");
+		}
+		return _alBooleanOperators;
+	}
+	
+	/**
+	 * This list contains all valid relational operators that may be mutated in a class.
+	 * @return <code>ArrayList</code> containing all relational operators available for mutations.
+	 */
+	private ArrayList<String> alRelationalOperators(){
+		if(_alRelationalOperators == null){
+			_alRelationalOperators = new ArrayList<String>();
+			_alRelationalOperators.add("==");_alRelationalOperators.add("!=");_alRelationalOperators.add("<=");
+			_alRelationalOperators.add(">=");_alRelationalOperators.add("<");_alRelationalOperators.add(">");
+		}
+		return _alRelationalOperators;
+	}
+	
+	/**
+	 * This list contains all valid arithmetic operators that may be mutated in a class.
+	 * @return <code>ArrayList</code> containing all arithmetic operators available for mutations.
+	 */
+	private ArrayList<String> alArithmeticOperators(){
+		if(_alArithmeticOperators == null){
+			_alArithmeticOperators = new ArrayList<String>();
+			_alArithmeticOperators.add("+");_alArithmeticOperators.add("-");_alArithmeticOperators.add("*");_alArithmeticOperators.add("/");_alArithmeticOperators.add("%");
+		}
+		return _alArithmeticOperators;
+	}
+	
+	/**
+	 * Determines if sOperator is an element in the list alOperatorsList. The list itself
+	 * will determine the type of operator you are checking sOperator to be.
+	 * @param <code>alOperatorsList</code> The list of valid operators to be checked.
+	 * @param <code>sOperator</code> The operator to be tested against the valid operators list.
+	 * @return <code> true </code> if the operator is in the valid operators list 'alOperatorsList'
+	 * 		   <code> false </code> otherwise
+	 */
+	private boolean isValidOperator(ArrayList<String> alOperatorsList, String sOperator){
+		if(alOperatorsList.indexOf(sOperator) != -1){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	
+	/**
+	 * Parses the arguments passed from the "main" method.
+	 * These arguments should contain the class name, and the operators.
+	 *
+	 * @param args program execution command line arguments
+	 */
+	private void parseArguments(String[] args) {
+		//Get the class file
+		try {
+			oClass = Repository.lookupClass(args[0]);
+			oClassGen = new ClassGen(oClass);
+		} catch (Exception e) {
+			showError("Please specify a valid class file.");
+		}
+		
+		//Get the operators
+		try {
+			//Store the operators, and trim any single quotes (needed to pass *, <, >, etc. as an argument)
+			sOldOperator = args[1].replace("'", "");
+			sNewOperator = args[2].replace("'", "");
+			//Having sOldOperator equal '*' and sNewOperator equal '>' is an example of type mismatch,
+			//or an unacceptable combination of operators. 
+			boolean operatorTypesMismatch = true;								
+			
+			if (!isValidOperator(alValidOperators(), sOldOperator) || !isValidOperator(alValidOperators(), sNewOperator)) {
+				showError("Invalid operator(s) specified.");
+			}
+			else if (sOldOperator.equals(sNewOperator)) {
+				showError("Old and New operators cannot be the same operator.");
+			}
+			else if ( isValidOperator(alArithmeticOperators(), sOldOperator) && isValidOperator(alArithmeticOperators(), sNewOperator)) {
+				operatorTypesMismatch = false;
+			}
+			else if ( isValidOperator(alBooleanOperators(), sOldOperator) && isValidOperator(alBooleanOperators(), sNewOperator)){
+				operatorTypesMismatch = false;
+			}
+			else if ( isValidOperator(alRelationalOperators(), sOldOperator) && isValidOperator(alRelationalOperators(), sNewOperator)){
+				operatorTypesMismatch = false;
+			}
+			if (operatorTypesMismatch){
+				showError("Operators type mismatch. Refer to manual for possible operator combinations.");
+			}
+		} catch (Exception e) {
+			showError("Please specify an old operator and a new operator.");
+		}
+	}
+	
+	/**
+	 * Prints the usage specifications of the program.
+	 */
+	private void printUsage() {
+		System.out.println("<Usage> java Mutator <class-file> <oldOp> <newOp>");
+		System.out.println("<class-file>: without extension");
+		System.out.println("<oldOp>: See Manual for Details");
+		System.out.println("<newOp>: See Manual for Details");
+	}
+	
+	/**
+	 * Shows an error to the user, prints the program usage, and exits.
+	 * 
+	 * @param sError the error to display to the user
+	 */
+	private void showError(String sError) {
+		System.out.println(sError);
+		printUsage();
+		System.exit(1);
+	}
+	
+	/**
+	 * Changes all instances of the old operator to the new operator
+	 * within a specific class file.
+	 */
+	public void changeOperators() {
+		int nMutations = 0;
+		System.out.println("Changing all instances of \"" + sOldOperator + "\" to \"" + sNewOperator + "\" in " + oClass.getClassName());
+		
+		oConstantPoolGen = oClassGen.getConstantPool();
+		arMethods = oClassGen.getMethods();
+		
+		for (int i=0; i<arMethods.length; i++) {
+			// mutation process
+			// 1. Go through all instructions in a method
+			// 2. If an instruction is an arithmetic instruction, see if it matches the old operator
+			// 3. If it matches the old operator, change the instruction to match the new operator, and set the isntruction
+			// 4. otherwise, don't worry about it and move on!
+			
+			MethodGen oMethodGen = new MethodGen(arMethods[i], oClass.getClassName(), oConstantPoolGen);
+			InstructionList il = oMethodGen.getInstructionList();
+			InstructionHandle ih;
+			for (ih = il.getStart(); ih != null; ih = ih.getNext()) {
+				Instruction oInstruction = ih.getInstruction();
+				
+				if (oInstHelper.isArithmeticInstruction(oInstruction)) {
+					if (oInstHelper.InstructionMatchOperator(oInstruction, sOldOperator)) {
+						//convert to the new instruction
+						oInstruction = oInstHelper.ConvertArithmetic(oInstruction, sNewOperator);
+						ih.setInstruction(oInstruction);					
+						nMutations++;
+					}					
+				}
+				else if(oInstHelper.isBranchInstruction(oInstruction)){
+					BranchInstruction oBranchInstruction = (BranchInstruction) oInstruction;
+					if (oInstHelper.InstructionMatchOperator(oInstruction, sOldOperator)) {
+						oBranchInstruction = oInstHelper.ConvertBranch(oBranchInstruction, sNewOperator);
+						ih.setInstruction(oBranchInstruction);
+						nMutations++;
+					}
+				}
+			}
+			il.setPositions();
+			oMethodGen.setInstructionList(il);
+			oClassGen.setMethodAt(oMethodGen.getMethod(), i);
+			il.dispose();
+		}		
+		System.out.println("Mutation complete. " + nMutations + " mutation(s) were performed.");	
+	}	
+	
+	/**
+	 * Dumps the modified class to the program directory.
+	 */
+	public void dumpClass() {
+		try {
+			oClassGen.getJavaClass().dump(oClass.getClassName() + ".class");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	/**
+	 * Offers the main method of program execution.  This is where
+	 * a call to changeOperators and dumpClass should be made.
+	 *
+	 * @param args command line arguments
+	 */
+	public static void main(String args[]) {
+		Mutator oMutator = new Mutator(args);
+		oMutator.changeOperators();
+		oMutator.dumpClass();
+	}
+}
--- trunk/mutation_tool/src/mutations/cInstructionHelper.java
+++ trunk/mutation_tool/src/mutations/cInstructionHelper.java
@@ -8,9 +8,9 @@ import org.apache.bcel.generic.BranchInstruction;
 import org.apache.bcel.Constants;
 
 /**
- * cInstructionHelper is a helper class for the ChangeOp program.
- * This class handles all operator/instruction verification.
- *
+ * cInstructionHelper is a helper class for the Mutator class.
+ * This class handles all operator/instruction verification, and
+ * changing of operators.
  * @author teh_code
  * @version 1.0
  */
@@ -175,7 +175,7 @@ public class cInstructionHelper {
 		else if (sOperator.equals("*")) { return "mul"; }
 		else if (sOperator.equals("/")) { return "div"; }
 		else if (sOperator.equals("%")) { return "rem"; }
-		else if (sOperator.equals("==")) { return "ne"; }
+		else if (sOperator.equals("==")) { return "ne"; }		//These instructions are opposite 
 		else if (sOperator.equals("!=")) { return "eq"; }
 		else if (sOperator.equals(">") ) { return "le"; }
 		else if (sOperator.equals(">=")) { return "lt"; }
