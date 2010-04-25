a7a44bc02692ca90da6866b78dcd3c188f086e45
curtis-johnson <curtis-johnson@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-25 08:25:06 +0000

    git-svn-id: https://cjfileserver/svn/teh_code@72 93af362e-f9d5-9440-b008-9d94dd279ff6

--- trunk/mutation_tool/src/controls/table/MutationTable.java
+++ trunk/mutation_tool/src/controls/table/MutationTable.java
@@ -1,5 +1,8 @@
 package controls.table;
 
+import interfaces.IMutableObject;
+import interfaces.IMutableTreeListener;
+
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.ComponentOrientation;
@@ -8,6 +11,7 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
@@ -16,28 +20,32 @@ import javax.swing.JPanel;
 import javax.swing.event.AncestorEvent;
 import javax.swing.event.AncestorListener;
 
-public class MutationTable extends JPanel implements ActionListener {
+import utilities.ClassLoader;
 
+import mutations.Mutant;
+import mutations.MutationFactory;
+
+import controls.MutableNode;
+
+public class MutationTable extends JPanel implements ActionListener, IMutableTreeListener {
+
+	private ArrayList<MutationRow> alMutableRows;
+	
 	public MutationTable() {
+		this.alMutableRows = new ArrayList<MutationRow>();
 		this.setSize(400, 100);
 		
-		//panel2.add(chk2);
-		//panel2.setMinimumSize(new Dimension(400, 50));
-		//panel2.setPreferredSize(new Dimension(400, 50));
-		//panel2.setMaximumSize(new Dimension(400, 50));
-		for (int i=0; i<5000; i++) {
-			if (i%2 == 0) {
-				MutationRow oRow = new MutationRow("Name", false, "ARITHMETIC", "*", "/", "CLASS", false, this);
-				this.add(oRow);
-			} else {
-				MutationRow oRow = new MutationRow("Name", false, "Logical", "AND", "OR", "CLASS", true, this);
-				this.add(oRow);
-			}
-		}
-		
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		loadTableWithClass();
 		
 		
+	}
+	
+	private void showAllMutableRows() {
+		this.removeAll();
+		for (int i=0; i<alMutableRows.size(); i++) {
+			this.add(alMutableRows.get(i));
+		}
 	}
 	
 	public void getSelectedMutationCount() {
@@ -56,6 +64,31 @@ public class MutationTable extends JPanel implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		getSelectedMutationCount();		
+	}
+	
+	public void loadTableWithClass() {
+		ArrayList<IMutableObject> alMutableObjects = MutationFactory.createIMutableObjects(ClassLoader.loadClassFromPath("C:\\Users\\cjohnson\\Documents\\Class Documents\\Spring 07\\CSE155\\HW #6\\CasualEmployee.class"));
+		this.alMutableRows.clear();
+		this.removeAll();
+		for (int i=0; i<alMutableObjects.size(); i++) {
+			IMutableObject oMutableObject = alMutableObjects.get(i);
+			boolean bAltRow = (i%2 == 0) ? false : true;
+			alMutableRows.add(new MutationRow(oMutableObject.getMutableClass().getClassName(), false, oMutableObject.getMutantTypeAsString(), oMutableObject.getOldOperator(),
+												oMutableObject.getNewOperator(), oMutableObject.getMutantLevelAsString(), bAltRow , this));
+		}
+		showAllMutableRows();
+	}
+
+	@Override
+	public void mutableNodeSelectionChanged(MutableNode oSelectedNode) {
+		ArrayList<IMutableObject> alMutableObjects = MutationFactory.createIMutableObjects(oSelectedNode.getMutableClass());
+		this.alMutableRows.clear();
+		for (int i=0; i<alMutableObjects.size(); i++) {
+			IMutableObject oMutableObject = alMutableObjects.get(i);
+			boolean bAltRow = (i%2 == 0) ? false : true;
+			alMutableRows.add(new MutationRow(oMutableObject.getMutableClass().getClassName(), false, oMutableObject.getMutantTypeAsString(), oMutableObject.getOldOperator(),
+												oMutableObject.getNewOperator(), oMutableObject.getMutantTypeAsString(), bAltRow , this));
+		}
 	}
 
 }
