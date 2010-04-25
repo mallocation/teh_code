885f045ef70a8d5c5e46e71fbcc1bedc31fdc5b9
pavel-patino <pavel-patino@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-25 04:00:04 +0000

    git-svn-id: https://cjfileserver/svn/teh_code@69 93af362e-f9d5-9440-b008-9d94dd279ff6

--- trunk/mutation_tool/src/mutations/testsDriver.java
+++ trunk/mutation_tool/src/mutations/testsDriver.java
@@ -1,25 +1,37 @@
 package mutations;
+
 import mutations.Mutator;
 import interfaces.IMutableObject;
-import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.JavaClass;
-import java.io.*;
-
+import utilities.ClassLoader;
+import mutations.MutationFactory;
+import java.util.*;
 
 public class testsDriver{
 	
-	private void showError(String sError) {
+	private static void showError(String sError) {
 		System.out.println(sError);
 		System.exit(1);
 	}
-	
-	public void main(String args[]){
+	public static void main(String args[]){
+		String path = "/Users/Pavel/Documents/workspace/mutation_tool/src/mutations/";
+
+		System.out.println(args.length);
+		System.out.println("" + args[0] + "" + args[1] + "" + args[2]);
+
 		IMutableObject oMutant = new Mutant();
 		JavaClass oClass = null;
 		String oldOp = null;
 		String newOp = null;
 		try{
-			oClass = Repository.lookupClass(args[0]);
+			String sClassWithPath = path + args[0] + ".class";
+			if(ClassLoader.isClassFile(sClassWithPath)){
+				oClass = ClassLoader.loadClassFromPath(sClassWithPath);
+
+			}
+			else{
+				showError("Not a valid class file.");
+			}
 		}
 		catch(Exception e){
 			showError("Specify valid class file.");
@@ -37,6 +49,14 @@ public class testsDriver{
 		Mutator oMutator = new Mutator(oMutant);
 		System.out.println("" + oMutator.getMutationCount());
 		oMutator.performMutation();
+		MutationFactory oFactory = new MutationFactory();
+		ArrayList<IMutableObject> mutableObjectsList = oFactory.createIMutableObjects(oClass);
+		Iterator iterator = mutableObjectsList.iterator();
+		
+		while(iterator.hasNext()){
+			((IMutableObject) iterator.next()).printMutableObjectProperties();
+		}
+		
 		
 	}
 }
