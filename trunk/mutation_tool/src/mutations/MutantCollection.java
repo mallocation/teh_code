8e45a0fde790101415b9fc4485bdaf06b394fe84
curtis-johnson <curtis-johnson@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-27 00:00:39 +0000

    git-svn-id: https://cjfileserver/svn/teh_code@119 93af362e-f9d5-9440-b008-9d94dd279ff6

--- trunk/mutation_tool/src/mutations/MutantCollection.java
+++ trunk/mutation_tool/src/mutations/MutantCollection.java
@@ -3,23 +3,59 @@ package mutations;
 import interfaces.IMutableObject;
 
 import java.util.ArrayList;
-// TODO Implement this class in place of array lists.
+
+/**
+ * This class is used to hold mutants to be passed throughout different
+ * modules in the mutation tool program.
+ * 
+ * @author teh_code
+ */
 public class MutantCollection {
 	
+	/**
+	 * Inner collection of mutants (This class is basically a wrapper around an array list!)
+	 */
 	private ArrayList<IMutableObject> alMutants;
 	
+	/**
+	 * Create a new empty collection.
+	 */
 	public MutantCollection() {
 		this.alMutants = new ArrayList<IMutableObject>();
 	}
 	
+	/**
+	 * Create a new collection with the specific contents.
+	 * 
+	 * @param alMutants collection to contain
+	 */
 	public MutantCollection(ArrayList<IMutableObject> alMutants) {
 		this.alMutants = alMutants;
 	}
 	
+	/**
+	 * Add an IMutableObject to the collection
+	 * 
+	 * @param oMutant mutant to add
+	 */
+	public void addMutant(IMutableObject oMutant) {
+		this.alMutants.add(oMutant);
+	}
+	
+	/**
+	 * Return the mutants in this collection.
+	 * 
+	 * @return ArrayList of IMutableObjects
+	 */
 	public ArrayList<IMutableObject> getMutants() {
 		return this.alMutants;
 	}
 	
+	/**
+	 * Return the number of mutants in this collection.
+	 * 
+	 * @return count of mutants
+	 */
 	public int getCollectionCount() {
 		return this.alMutants.size();
 	}
