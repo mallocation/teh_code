b4a605591c08db6b8aef613e520d1844079b3457
curtis-johnson <curtis-johnson@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-27 12:21:19 +0000

    git-svn-id: https://cjfileserver/svn/teh_code@166 93af362e-f9d5-9440-b008-9d94dd279ff6

---
+++ trunk/mutation_tool/src/interfaces/IMutationRowListener.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public interface IMutationRowListener {
+	
+	public void mutableObjectSelected(IMutableObject oMutant);
+	
+	public void mutableObjectUnSelected(IMutableObject oMutant);
+
+}
