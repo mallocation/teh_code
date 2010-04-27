161da5f1eccd8923f16b289bbaf3311806e8f7ea
tyler-hawkins <tyler-hawkins@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-27 05:22:31 +0000

    git-svn-id: https://cjfileserver/svn/teh_code@137 93af362e-f9d5-9440-b008-9d94dd279ff6

--- trunk/mutation_tool/src/mutations/MutationsSelectedRow.java
+++ trunk/mutation_tool/src/mutations/MutationsSelectedRow.java
@@ -1,26 +1,29 @@
 package mutations;
 
+import interfaces.IMutableObject;
+
 import java.awt.*;
 import javax.swing.*;
 
+import org.apache.bcel.classfile.JavaClass;
+
 public class MutationsSelectedRow extends JPanel {
-	public String className;
+	public JavaClass oClass;
 	public String mutationCount;
 	public boolean exported;
 	public String imagePath;
 	public ImageIcon image;
 	public MutantCollection mutant;
 	
-	public MutationsSelectedRow(String name, MutantCollection mutants){
-		this.className = name;
+	public MutationsSelectedRow(JavaClass rClass){
+		this.oClass = rClass;
 		this.exported = false;
 		this.imagePath = "../images/ClassTree.png";
-		this.mutant = mutants;
+		this.mutant = new MutantCollection();
 		//this.mutationCount = Integer.toString(mutants.getCollectionCount());
 		
-		JLabel cn = new JLabel(this.className);
-		//JLabel mc = new JLabel(" (Mutation Count: " + this.mutationCount + ")");
-		JLabel mc = new JLabel(" (Mutation Count: 1111)");
+		JLabel cn = new JLabel(rClass.toString());
+		JLabel mc = new JLabel(" (Mutation Count: " + this.mutant.getCollectionCount() + ")");
 		JLabel im = new JLabel(new ImageIcon(this.getClass().getResource(this.imagePath)));
 		
 		this.add(im);
@@ -28,12 +31,16 @@ public class MutationsSelectedRow extends JPanel {
 		this.add(mc);
 	}
 	
-	public String getClassName(){
-		return this.className;
+	public JavaClass getJavaClass(){
+		return this.oClass;
 	}
 	
 	public String getMutationCount(){
 		return this.mutationCount;
+	}
+	
+	public MutantCollection getMutationCollection(){
+		return this.mutant;
 	}
 	
 	public boolean isExported(){
@@ -45,5 +52,13 @@ public class MutationsSelectedRow extends JPanel {
 			image = new ImageIcon(imagePath);
 		}
 		return image;
+	}
+	
+	public void addMutableObj(IMutableObject mObject){
+		this.mutant.addMutant(mObject);
+	}
+	
+	public void removeMutableObj(IMutableObject mObject){
+		this.mutant.removeMutant(mObject);
 	}
 }
