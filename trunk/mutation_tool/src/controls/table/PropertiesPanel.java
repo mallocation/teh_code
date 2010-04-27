4f1688302db8cb364d921c391fdb766906a6a063
brett-baumert <brett-baumert@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-27 06:34:34 +0000

    git-svn-id: https://cjfileserver/svn/teh_code@149 93af362e-f9d5-9440-b008-9d94dd279ff6

--- trunk/mutation_tool/src/controls/table/PropertiesPanel.java
+++ trunk/mutation_tool/src/controls/table/PropertiesPanel.java
@@ -3,27 +3,50 @@ package controls.table;
 import interfaces.IMutableObject;
 import interfaces.IMutationRowActor;
 
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import mutations.Mutator;
 
 public class PropertiesPanel extends JPanel implements IMutationRowActor{
 	
 	public Dimension propertiesPanelDimension;
+	public JTextArea propertiesTextArea;
+	public String mutationCountText;
 	
 	public PropertiesPanel(){
-		propertiesPanelDimension = new Dimension(554,200);
+		propertiesPanelDimension = new Dimension(554,120);
 		this.setPreferredSize(propertiesPanelDimension);
 		this.setMaximumSize(propertiesPanelDimension);
 		this.setMinimumSize(propertiesPanelDimension);
 		this.setBorder(BorderFactory.createTitledBorder("Properties Panel"));
+		propertiesTextArea = new JTextArea(10,45);
+		propertiesTextArea.setText("Mutation Count: \n");
+		propertiesTextArea.append("Mutant Level: \n");
+		propertiesTextArea.append("Method Name: \n");
+		propertiesTextArea.append("Mutant Type: \n");
+		propertiesTextArea.append("Operator Change: ");
+		propertiesTextArea.setOpaque(false);
+		propertiesTextArea.setEditable(false);
+		this.add(propertiesTextArea);
 	}
 
 	@Override
 	public void updatePropertiesPanel(IMutableObject oMutant) {
 		// TODO Auto-generated method stub
-		System.out.println("aldkfjlksdf");
+		mutationCountText = Integer.toString(Mutator.getMutationCount(oMutant));
+		propertiesTextArea.setText("Mutation Count: " + mutationCountText + "\n");
+		propertiesTextArea.append("Mutant Level: " + oMutant.getMutantLevelAsString() + "\n");
+		if(oMutant.getMethodName() == null){
+			oMutant.setMethodName("N/A");
+		}
+		propertiesTextArea.append("Method Name: " + oMutant.getMethodName() + "\n");
+		propertiesTextArea.append("Mutant Type: " + oMutant.getMutantTypeAsString() + "\n");
+		propertiesTextArea.append("Operator Change: " + oMutant.getOldOperator() + " to " + oMutant.getNewOperator());
 		
 	}
 }
