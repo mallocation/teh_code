3ccf3eb334326f5016233b66202cda07512f4555
curtis-johnson <curtis-johnson@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-27 07:41:32 +0000

    git-svn-id: https://cjfileserver/svn/teh_code@153 93af362e-f9d5-9440-b008-9d94dd279ff6

--- trunk/mutation_tool/src/controls/table/MutationTableFilter.java
+++ trunk/mutation_tool/src/controls/table/MutationTableFilter.java
@@ -4,6 +4,8 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
@@ -12,15 +14,18 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import mutations.Mutant;
 import mutations.cInstructionHelper;
+import interfaces.IMutationFilterActor;
 import interfaces.IMutationFilterListener;
 
-public class MutationTableFilter extends JPanel implements ActionListener {
-	private JLabel lblMutationType, lblMutatedOp, lblNewOp;
+public class MutationTableFilter extends JPanel implements ActionListener, KeyListener, IMutationFilterActor {
+	private JLabel lblMutationType, lblMutatedOp, lblNewOp, lblSearch;
 	private JComboBox ddlMutationType, ddlMutatedOp, ddlNewOp;
 	private JCheckBox chkSelectAll;
+	private JTextField txtSearch;
 	private Dimension oTableDimension;
 	private ArrayList<IMutationFilterListener> alFilterListeners;
 	
@@ -29,25 +34,50 @@ public class MutationTableFilter extends JPanel implements ActionListener {
 		lblMutationType = new JLabel("Mutation Type");
 		lblMutatedOp = new JLabel("Mutated Operation");
 		lblNewOp = new JLabel("New Operation");
+		lblSearch = new JLabel("Search");
 		
 		ddlMutationType = new JComboBox(Mutant.getAllMutantTypesAsString());
 		ddlMutationType.insertItemAt("", 0);
 		ddlMutationType.setSelectedIndex(0);
 		ddlMutatedOp = new JComboBox();
 		ddlNewOp = new JComboBox();
+		
+		txtSearch = new JTextField();
 	
 		chkSelectAll = new JCheckBox("Select All");
 		
 		oTableDimension = new Dimension(565, 90);
-		
+		this.setLayout(null);
 		
 		this.add(lblMutationType);
+		lblMutationType.setBounds(10, 20, 90, 30);
+		
 		this.add(ddlMutationType);
+		ddlMutationType.setBounds(95, 25, 100, 22);
+		
 		this.add(lblMutatedOp);
+		//lblMutatedOp.setBounds(395, 10, 107, 30);
+		lblMutatedOp.setBounds(225, 20, 107, 30);
+		
 		this.add(ddlMutatedOp);
+		//ddlMutatedOp.setBounds(507, 15, 50, 22);
+		ddlMutatedOp.setBounds(337, 25, 50, 22);
+		
 		this.add(lblNewOp);
+		lblNewOp.setBounds(416, 20, 107, 30);
+		
 		this.add(ddlNewOp);
+		ddlNewOp.setBounds(507, 25, 50, 22);
+		
 		this.add(chkSelectAll);
+		chkSelectAll.setBounds(480, 55, 80, 30);
+		
+		this.add(lblSearch);
+		lblSearch.setBounds(45, 55, 70, 30);
+		
+		this.add(txtSearch);
+		txtSearch.setBounds(95, 60, 240, 22);
+		
 		this.setPreferredSize(oTableDimension);
 		this.setMinimumSize(oTableDimension);
 		this.setMaximumSize(oTableDimension);
@@ -55,7 +85,7 @@ public class MutationTableFilter extends JPanel implements ActionListener {
 		ddlMutationType.addActionListener(this);
 		ddlMutatedOp.addActionListener(this);
 		ddlNewOp.addActionListener(this);
-		
+		txtSearch.addKeyListener(this);		
 		chkSelectAll.addActionListener(this);
 		
 		this.alFilterListeners = new ArrayList<IMutationFilterListener>();		
@@ -72,13 +102,14 @@ public class MutationTableFilter extends JPanel implements ActionListener {
 	}
 	
 	private void fireFilterListeners() {
-		String mutationType, oldOp, newOp;
+		String mutantSearch, mutationType, oldOp, newOp;
+		mutantSearch = txtSearch.getText().trim();
 		mutationType = ddlMutationType.getSelectedItem() == null ? "" : ddlMutationType.getSelectedItem().toString();
 		oldOp = ddlMutatedOp.getSelectedItem() == null ? "" : ddlMutatedOp.getSelectedItem().toString();
 		newOp = ddlNewOp.getSelectedItem() == null ? "" : ddlNewOp.getSelectedItem().toString();
 		
 		for (int i=0; i<alFilterListeners.size(); i++) {
-			this.alFilterListeners.get(i).filterMutations(mutationType, oldOp, newOp);
+			this.alFilterListeners.get(i).filterMutations(mutantSearch, mutationType, oldOp, newOp);
 		}		
 	}
 
@@ -106,6 +137,36 @@ public class MutationTableFilter extends JPanel implements ActionListener {
 			}
 			fireFilterListeners();
 		}
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// do nothing
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		fireFilterListeners();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		//fireFilterListeners();		
+	}
+
+	@Override
+	public void resetMutationFilter() {
+		ddlMutationType.removeAllItems();
+		String[] arMutationTypes = Mutant.getAllMutantTypesAsString();
+		ddlMutationType.insertItemAt("", 0);
+		ddlMutationType.setSelectedIndex(0);
+		ddlMutatedOp = new JComboBox();
+		ddlNewOp = new JComboBox();
+		
+		txtSearch = new JTextField();
+	
+		chkSelectAll = new JCheckBox("Select All");
 		
 	}
 	
