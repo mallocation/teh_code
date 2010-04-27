b08872b87ef1a0fe6ffd9ceed733181f55852eb0
tyler-hawkins <tyler-hawkins@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-27 06:22:01 +0000

    git-svn-id: https://cjfileserver/svn/teh_code@147 93af362e-f9d5-9440-b008-9d94dd279ff6

--- trunk/mutation_tool/src/mutations/MutationsSelectedRow.java
+++ trunk/mutation_tool/src/mutations/MutationsSelectedRow.java
@@ -15,15 +15,21 @@ public class MutationsSelectedRow extends JPanel {
 	public ImageIcon image;
 	public MutantCollection mutant;
 	
-	public MutationsSelectedRow(JavaClass rClass){
+	public MutationsSelectedRow(JavaClass rClass, boolean isExported){
 		this.oClass = rClass;
-		this.exported = false;
-		this.imagePath = "../images/ClassTree.png";
+		this.exported = isExported;
+		if(isExported){
+			this.imagePath = "../images/green_dot.png";
+		} else{
+			this.imagePath = "../images/yellow_dot.png";
+		}
 		this.mutant = new MutantCollection();
 		//this.mutationCount = Integer.toString(mutants.getCollectionCount());
 		
-		JLabel cn = new JLabel(rClass.toString());
-		JLabel mc = new JLabel(" (Mutation Count: " + this.mutant.getCollectionCount() + ")");
+		//JLabel cn = new JLabel(rClass.toString());
+		JLabel cn = new JLabel("ClassName1");
+		//JLabel mc = new JLabel(" (Mutation Count: " + this.mutant.getCollectionCount() + ")");
+		JLabel mc = new JLabel(" (Mutation Count: 1111)");
 		JLabel im = new JLabel(new ImageIcon(this.getClass().getResource(this.imagePath)));
 		
 		this.add(im);
