6f7497c0ac32219a032c2d3916b8c7b0125da959
curtis-johnson <curtis-johnson@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-30 13:36:37 +0000

    git-svn-id: https://cjfileserver/svn/teh_code@203 93af362e-f9d5-9440-b008-9d94dd279ff6

--- trunk/mutation_tool/src/InterfaceSelection.java
+++ trunk/mutation_tool/src/InterfaceSelection.java
@@ -1,3 +1,5 @@
+import java.awt.GraphicsEnvironment;
+
 import javax.swing.SwingUtilities;
 import utilities.ParseXML;
 
@@ -6,24 +8,17 @@ public class InterfaceSelection {
 	
     public static void main (String args[])  {
     	
-    	if(args.length == 0){
+    	if(args.length == 0 && !GraphicsEnvironment.isHeadless()){
 			SwingUtilities.invokeLater(new Runnable() {			
 				public void run() {
 					new MutationTool();
 				}			
 			});	
-    	}
-    	
-    	else {
+    	}	else {
     		ParseXML commandLineInterface = new ParseXML();
     		for(int i=0; i < args.length; i++){
     			commandLineInterface.getMutationsFromCommandLine(args[i]);
     		}
-    		
-    	}
-    	
-    	
+    	}    	
     }
-    	
-
 }
