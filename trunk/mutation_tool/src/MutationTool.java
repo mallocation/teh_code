4d7b4902ea9316e56c28bf4703d0b05482cec8ff
curtis-johnson <curtis-johnson@93af362e-f9d5-9440-b008-9d94dd279ff6>
2010-04-30 20:03:17 +0000

    git-svn-id: https://cjfileserver/svn/teh_code@239 93af362e-f9d5-9440-b008-9d94dd279ff6

---
+++ trunk/mutation_tool/src/MutationTool.java
@@ -0,0 +1,32 @@
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.SwingUtilities;
+
+import utilities.ParseXML;
+
+
+public class MutationTool {
+	
+    public static void main (String args[])  {
+    	
+    	if(args.length == 0 && !GraphicsEnvironment.isHeadless()){
+			SwingUtilities.invokeLater(new Runnable() {			
+				public void run() {
+					new teh_code.gui.MutationTool();
+				}			
+			});	
+    	}	else {
+    		ParseXML commandLineInterface = new ParseXML();
+    		
+    		if (args.length == 0) {
+    			for(int i=0; i < args.length; i++){
+    				commandLineInterface.getMutationsFromCommandLine(args[i]);  			
+    			}
+    		} else {
+    			//print usage
+    			System.out.println("Usage: MutationTool file1 file2 file3");
+    			System.out.println("where file1, file2, etc are compatible mutation scripting xml files");    			
+    		}
+    	}    	
+    }
+}
